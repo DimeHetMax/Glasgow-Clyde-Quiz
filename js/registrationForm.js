27652bf297ec67e42df441c5f6092daf3d5d1bd0
Maksym Dimekhin <dimehinm@gmail.com>
2026-05-04 23:28:29 +0100

    edite

--- js/registrationForm.js
+++ js/registrationForm.js
@@ -8,7 +8,7 @@ const handForm = (e)=>{
  const email = form.email.value.trim()
  console.log({name, email});
  setCookie({name, email})
- window.location.href = "/pages/menu.html"
+ window.location.href = "./pages/menu.html"
  formEl.reset()
 }
 formEl.addEventListener("submit",handForm)
