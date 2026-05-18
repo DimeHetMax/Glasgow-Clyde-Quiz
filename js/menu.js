1de754331e021993a3c7bf838ff765977cc69f2f
Maksym Dimekhin <20361352@myclyde.ac.uk>
2026-05-18 13:41:20 +0100

    adding styles,changing menujs,sprite

--- js/menu.js
+++ js/menu.js
@@ -9,20 +9,34 @@ const menuTitle = document.querySelector(".menu-title");
 const menuQuizList = document.querySelector(".menu-quiz-list");
 const switchBtn = document.querySelector(".switch");
 
-
 const userName = getCookie();
 if (!userName) {
   locateTo("../index.html");
 }
 
 addLogoutBtn(switchBtn, userName);
-logout(userName)
-welcomeMessage(menuTitle, userName)
+logout(userName);
+welcomeMessage(menuTitle, userName);
 
 // menuTitle.textContent = `Welcome, ${userName}! To Quiz !`;
-quizzes.forEach(({ id, image, title }) => {
-  const html = `
-               <li class="menu-quiz-item" id=${id}>
+// quizzes.forEach(({ id, image, title }) => {
+//   const html = `
+//                <li class="menu-quiz-item" id=${id}>
+//                <a href="../pages/quiz.html?quiz=${title.toLowerCase().split(" ").join("-")}">
+//                   <div class="menu-quiz-wrap-img">
+//                     <img class="menu-quiz-img" src=${image} alt=${title}>
+//                   </div>
+//                   <div class="menu-quiz-wrap-text">
+//                     <p class="menu-quiz-text">${title}</p>
+//                   </div>
+//                   </a>
+//                 </li>
+// `;
+//   menuQuizList.insertAdjacentHTML("afterbegin", html);
+// });
+const arrQuiz = quizzes
+  .map(({ id, image, title }) => {
+    return `<li class="menu-quiz-item" id=${id}>
                <a href="../pages/quiz.html?quiz=${title.toLowerCase().split(" ").join("-")}">
                   <div class="menu-quiz-wrap-img">
                     <img class="menu-quiz-img" src=${image} alt=${title}>
@@ -31,12 +45,8 @@ quizzes.forEach(({ id, image, title }) => {
                     <p class="menu-quiz-text">${title}</p>
                   </div>
                   </a>
-                </li>
-`;
-  menuQuizList.insertAdjacentHTML("afterbegin", html);
-});
-
+                </li>`;
+  })
+  .join("");
+menuQuizList.insertAdjacentHTML("afterbegin", arrQuiz);
 // {id: 1, title: 'Basic Numeracy', image: './image/numeracy.jpg', questions: Array(5)}
-
-
-
